900a33fb567b75ecfa16efa94203342e8a9186aa
Vytautas <vytautaskasp@gmail.com>
2017-08-19 17:25:11 +0300

    Initial commit with standard metrics working.

---
+++ sample-app/Startup.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Prometheus;
+using Prometheus.Advanced;
+
+namespace sample_app
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            var collectorRegistry = DefaultCollectorRegistry.Instance;
+            collectorRegistry.RegisterOnDemandCollectors(new [] {
+                new DotNetStatsCollector()
+            });
+
+            app.Map("/metrics", appConfig => {
+                appConfig.Run(async context =>
+                {
+                    var req = context.Request;
+                    var response = context.Response;
+
+                    var acceptHeader = req.Headers["Accept"];
+                    var contentType = ScrapeHandler.GetContentType(acceptHeader);
+
+                    response.ContentType = contentType;
+
+                    using (var outputStream = response.Body)
+                    {
+                        var collected = collectorRegistry.CollectAll();
+                        ScrapeHandler.ProcessScrapeRequest(collected, contentType, outputStream);
+                    }
+
+                    await Task.FromResult(0).ConfigureAwait(false);
+                });
+            });
+        }
+    }
+}
